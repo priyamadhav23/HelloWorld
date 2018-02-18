8210e27c997dd55f67ace8490d9374485f21b12a
Priya M <36603460+priyamadhav23@users.noreply.github.com>
2018-02-18 22:40:44 +0530

    Create ViewController.swift
    it helps to print hello world in console

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
